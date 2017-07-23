91192c74e7e065f27205fff482f846dfe3e73b73
XHfight <X_H_fight@163.com>
2017-07-24 00:53:48 +0800

    webbench源码注释

---
+++ WebBench剖析/webbench.c
@@ -0,0 +1,533 @@
+/*
+* (C) Radim Kolar 1997-2004
+* This is free software, see GNU Public License version 2 for
+* details.
+*
+* Simple forking WWW Server benchmark:
+*
+* Usage:
+*   webbench --help
+*
+* Return codes:
+*    0 - sucess
+*    1 - benchmark failed (server is not on-line)
+*    2 - bad param
+*    3 - internal error, fork failed
+* 
+*/ 
+
+#include "socket.c"
+#include <unistd.h>
+#include <sys/param.h>
+#include <rpc/types.h>
+#include <getopt.h>
+#include <strings.h>
+#include <time.h>
+#include <signal.h>
+
+/* values */
+volatile int timerexpired=0;
+int speed=0;  //所有请求的成功数
+int failed=0;  //所有请求的失败数
+int bytes=0;   //服务器向客户端发送的所有数据量
+
+/* globals */
+/*默认为http1.0版本*/
+int http10=1; /* 0 - http/0.9, 1 - http/1.0, 2 - http/1.1 */
+/*支持四种http请求方法*/
+/* Allow: GET, HEAD, OPTIONS, TRACE */
+#define METHOD_GET 0
+#define METHOD_HEAD 1
+#define METHOD_OPTIONS 2
+#define METHOD_TRACE 3
+#define PROGRAM_VERSION "1.5"
+
+//默认值
+int method=METHOD_GET;  //默认请求方法为get
+int clients=1;          //默认并发量（访问服务器的客户端数量）为1
+int force=0;            //默认等待服务器响应 
+int force_reload=0;		//默认失败后重新请求
+int proxyport=80;       //默认端口:80
+char *proxyhost=NULL;   //代理服务器
+int benchtime=30;       //不传-t参数，默认请求30s
+
+/* internal */
+int mypipe[2];    //管道：子进程向父进程传递speed、failed、bytes
+char host[MAXHOSTNAMELEN]; //要访问的主机域名或IP地址：url中//之后port之前的
+#define REQUEST_SIZE 2048
+char request[REQUEST_SIZE]; //存储请求报文
+
+//传给getopt_long函数的参数
+static const struct option long_options[]=
+{
+    {"force",no_argument,&force,1},
+    {"reload",no_argument,&force_reload,1},
+    {"time",required_argument,NULL,'t'},
+    {"help",no_argument,NULL,'?'},
+    {"http09",no_argument,NULL,'9'},
+    {"http10",no_argument,NULL,'1'},
+    {"http11",no_argument,NULL,'2'},
+    {"get",no_argument,&method,METHOD_GET},
+    {"head",no_argument,&method,METHOD_HEAD},
+    {"options",no_argument,&method,METHOD_OPTIONS},
+    {"trace",no_argument,&method,METHOD_TRACE},
+    {"version",no_argument,NULL,'V'},
+    {"proxy",required_argument,NULL,'p'},
+    {"clients",required_argument,NULL,'c'},
+    {NULL,0,NULL,0}
+};
+
+/* prototypes */
+static void benchcore(const char* host,const int port, const char *request);
+static int bench(void);
+static void build_request(const char *url);
+
+static void alarm_handler(int signal)
+{
+    timerexpired=1;
+}	
+
+//输出工具的正确用法
+static void usage(void)
+{
+    fprintf(stderr,
+            "webbench [option]... URL\n"
+            "  -f|--force               Don't wait for reply from server.\n"
+            "  -r|--reload              Send reload request - Pragma: no-cache.\n"
+            "  -t|--time <sec>          Run benchmark for <sec> seconds. Default 30.\n"
+            "  -p|--proxy <server:port> Use proxy server for request.\n"
+            "  -c|--clients <n>         Run <n> HTTP clients at once. Default one.\n"
+            "  -9|--http09              Use HTTP/0.9 style requests.\n"
+            "  -1|--http10              Use HTTP/1.0 protocol.\n"
+            "  -2|--http11              Use HTTP/1.1 protocol.\n"
+            "  --get                    Use GET request method.\n"
+            "  --head                   Use HEAD request method.\n"
+            "  --options                Use OPTIONS request method.\n"
+            "  --trace                  Use TRACE request method.\n"
+            "  -?|-h|--help             This information.\n"
+            "  -V|--version             Display program version.\n"
+           );
+}
+
+int main(int argc, char *argv[])
+{
+    int opt=0;
+    int options_index=0;
+    char *tmp=NULL;
+
+	//检查参数
+    if(argc==1)
+    {
+        usage();
+        return 2;
+    } 
+
+	//获取命令行参数，对各个参数进行处理
+    //getopt_long是命令行解析的库函数
+	while((opt=getopt_long(argc,argv,"912Vfrt:p:c:?h",long_options,&options_index))!=EOF )
+    {
+        switch(opt)
+        {
+            case  0 : break;
+            case 'f': force=1;break;
+            case 'r': force_reload=1;break; 
+            case '9': http10=0;break;
+            case '1': http10=1;break;
+            case '2': http10=2;break;
+            case 'V': printf(PROGRAM_VERSION"\n");exit(0);
+            case 't': benchtime=atoi(optarg);break;	     
+            case 'p': 
+            /* proxy server parsing server:port */
+            //strrchr查找字符最后一次出现的位置，返回该字符及其后面的字符。
+			tmp=strrchr(optarg,':');
+            proxyhost=optarg;
+            if(tmp==NULL)
+            {
+                break;
+            }
+            if(tmp==optarg)
+            {
+                fprintf(stderr,"Error in option --proxy %s: Missing hostname.\n",optarg);
+                return 2;
+            }
+            if(tmp==optarg+strlen(optarg)-1)
+            {
+                fprintf(stderr,"Error in option --proxy %s Port number is missing.\n",optarg);
+                return 2;
+            }
+            *tmp='\0';
+            proxyport=atoi(tmp+1);break;
+            case ':':
+            case 'h':
+            case '?': usage();return 2;break;
+            case 'c': clients=atoi(optarg);break;
+        }
+    }
+
+	/*optind被getopt_long函数设置为命令行参数中未读取的下一个元素下标值。
+	 *argc为命令行参数个数＋1
+	 *url（最后一个参数）应该放在argc－1的位置 */
+    if(optind==argc) {   //命令行参数已读完，说明缺少url
+        fprintf(stderr,"webbench: Missing URL!\n");
+        usage();
+        return 2;
+    }
+
+	/*当指定客户端和测试时间为0时，默认设置*/
+    if(clients==0) clients=1;
+    if(benchtime==0) benchtime=30;
+ 
+    /* Copyright */
+    fprintf(stderr,"Webbench - Simple Web Benchmark "PROGRAM_VERSION"\n"
+            "Copyright (c) Radim Kolar 1997-2004, GPL Open Source Software.\n"
+            );
+ 
+	/*构建http请求存入全局request数组中*/
+    build_request(argv[optind]);
+ 
+	/*打印提示信息*/
+    // print request info ,do it in function build_request
+    printf("Benchmarking: ");
+ 
+    switch(method)
+    {
+        case METHOD_GET:
+        default:
+        printf("GET");break;
+        case METHOD_OPTIONS:
+        printf("OPTIONS");break;
+        case METHOD_HEAD:
+        printf("HEAD");break;
+        case METHOD_TRACE:
+        printf("TRACE");break;
+    }
+    
+    printf(" %s",argv[optind]);
+    
+    switch(http10)
+    {
+        case 0: printf(" (using HTTP/0.9)");break;
+        case 2: printf(" (using HTTP/1.1)");break;
+    }
+ 
+    printf("\n");
+    
+
+    printf("Runing info: ");
+
+    if(clients==1) 
+        printf("1 client");
+    else
+        printf("%d clients",clients);
+
+    printf(", running %d sec", benchtime);
+    
+    if(force) printf(", early socket close");
+    if(proxyhost!=NULL) printf(", via proxy server %s:%d",proxyhost,proxyport);
+    if(force_reload) printf(", forcing reload");
+    
+    printf(".\n");
+    
+	//开始压力测试，返回压力测试结果
+    return bench();
+}
+
+//创建一个http请求报文存放在全局request数组中
+void build_request(const char *url)
+{
+    char tmp[10];
+    int i;
+
+    //bzero(host,MAXHOSTNAMELEN);
+    //bzero(request,REQUEST_SIZE);
+    memset(host,0,MAXHOSTNAMELEN);
+    memset(request,0,REQUEST_SIZE);
+
+    if(force_reload && proxyhost!=NULL && http10<1) http10=1;
+    if(method==METHOD_HEAD && http10<1) http10=1;
+    if(method==METHOD_OPTIONS && http10<2) http10=2;
+    if(method==METHOD_TRACE && http10<2) http10=2;
+
+    switch(method)
+    {
+        default:
+        case METHOD_GET: strcpy(request,"GET");break;
+        case METHOD_HEAD: strcpy(request,"HEAD");break;
+        case METHOD_OPTIONS: strcpy(request,"OPTIONS");break;
+        case METHOD_TRACE: strcpy(request,"TRACE");break;
+    }
+
+    strcat(request," ");  //字符串拼接，将第二个参数拼接在第一个后面
+	
+    if(NULL==strstr(url,"://"))
+    {
+        fprintf(stderr, "\n%s: is not a valid URL.\n",url);
+        exit(2);
+    }
+    if(strlen(url)>1500)
+    {
+        fprintf(stderr,"URL is too long.\n");
+        exit(2);
+    }
+    if (0!=strncasecmp("http://",url,7)) 
+    { 
+        fprintf(stderr,"\nOnly HTTP protocol is directly supported, set --proxy for others.\n");
+        exit(2);
+    }
+    
+    /* protocol/host delimiter */
+    i=strstr(url,"://")-url+3;
+
+    if(strchr(url+i,'/')==NULL) {
+        fprintf(stderr,"\nInvalid URL syntax - hostname don't ends with '/'.\n");
+        exit(2);
+    }
+    
+    if(proxyhost==NULL)
+    {
+        /* get port from hostname */
+        if(index(url+i,':')!=NULL && index(url+i,':')<index(url+i,'/'))
+        {
+            strncpy(host,url+i,strchr(url+i,':')-url-i);
+            //bzero(tmp,10);
+            memset(tmp,0,10);
+            strncpy(tmp,index(url+i,':')+1,strchr(url+i,'/')-index(url+i,':')-1);
+            /* printf("tmp=%s\n",tmp); */
+            proxyport=atoi(tmp);
+            if(proxyport==0) proxyport=80;
+        } 
+        else
+        {
+			//strcspn用于查找子串
+            strncpy(host,url+i,strcspn(url+i,"/"));
+        }
+        // printf("Host=%s\n",host);
+        strcat(request+strlen(request),url+i+strcspn(url+i,"/"));
+    } 
+    else
+    {
+        // printf("ProxyHost=%s\nProxyPort=%d\n",proxyhost,proxyport);
+        strcat(request,url);
+    }
+
+    if(http10==1)
+        strcat(request," HTTP/1.0");
+    else if (http10==2)
+        strcat(request," HTTP/1.1");
+  
+    strcat(request,"\r\n");
+  
+    if(http10>0)
+        strcat(request,"User-Agent: WebBench "PROGRAM_VERSION"\r\n");
+    if(proxyhost==NULL && http10>0)
+    {
+        strcat(request,"Host: ");
+        strcat(request,host);
+        strcat(request,"\r\n");
+    }
+ 
+    if(force_reload && proxyhost!=NULL)
+    {
+        strcat(request,"Pragma: no-cache\r\n");
+    }
+  
+    if(http10>1) //http1.1
+        strcat(request,"Connection: close\r\n");
+    
+    /* add empty line at end */
+    if(http10>0)  //http1.0 
+		strcat(request,"\r\n"); 
+    
+    printf("\nRequest:\n%s\n",request);
+}
+
+/* 进行测试 */
+/* vraci system rc error kod */
+static int bench(void)
+{
+    int i,j,k;	
+    pid_t pid=0;
+    FILE *f;
+
+	/*向服务器发起并关闭一次连接，测试服务器是否可以正常访问*/
+    /* check avaibility of target server */
+    i=Socket(proxyhost==NULL?host:proxyhost,proxyport);
+    if(i<0) { 
+        fprintf(stderr,"\nConnect to server failed. Aborting benchmark.\n");
+        return 1;
+    }
+    close(i);
+    
+    /* create pipe */
+    if(pipe(mypipe))
+    {
+        perror("pipe failed.");
+        return 3;
+    }
+
+    /* not needed, since we have alarm() in childrens */
+    /* wait 4 next system clock tick */
+    /*
+    cas=time(NULL);
+    while(time(NULL)==cas)
+    sched_yield();
+    */
+
+	/*fork创建子进程，数量：-c选项指定的并发数*/
+    /* fork childs */
+    for(i=0;i<clients;i++)
+    {
+        pid=fork();
+        if(pid <= (pid_t) 0)
+        {
+            /* child process or error*/
+            sleep(1); /* make childs faster */
+            break;  //子进程要立即跳出循环，不然子进程也会继续fork
+        }
+    }
+
+	//fork失败
+    if( pid < (pid_t) 0)
+    {
+        fprintf(stderr,"problems forking worker no. %d\n",i);
+        perror("fork failed.");
+        return 3;
+    }
+	
+	
+    if(pid == (pid_t) 0)  //子进程
+    {
+        /* I am a child */
+        if(proxyhost==NULL)
+            benchcore(host,proxyport,request);
+        else
+            benchcore(proxyhost,proxyport,request);
+
+        /* write results to pipe */
+		//fdopen将文件描述符转为文件指针
+        f=fdopen(mypipe[1],"w");
+        if(f==NULL)
+        {
+            perror("open pipe for writing failed.");
+            return 3;
+        }
+        /* fprintf(stderr,"Child - %d %d\n",speed,failed); */
+        fprintf(f,"%d %d %d\n",speed,failed,bytes);
+        fclose(f);
+
+        return 0;
+    } 
+    else //父进程
+    {
+        f=fdopen(mypipe[0],"r");
+        if(f==NULL) 
+        {
+            perror("open pipe for reading failed.");
+            return 3;
+        }
+        
+        setvbuf(f,NULL,_IONBF,0);
+        
+        speed=0;
+        failed=0;
+        bytes=0;
+    
+        while(1)
+        {
+            pid=fscanf(f,"%d %d %d",&i,&j,&k);
+            if(pid<2)
+            {
+                fprintf(stderr,"Some of our childrens died.\n");
+                break;
+            }
+            
+            speed+=i;
+            failed+=j;
+            bytes+=k;
+        
+            /* fprintf(stderr,"*Knock* %d %d read=%d\n",speed,failed,pid); */
+            if(--clients==0) break;
+        }
+    
+        fclose(f);
+
+        printf("\nSpeed=%d pages/min, %d bytes/sec.\nRequests: %d susceed, %d failed.\n",
+            (int)((speed+failed)/(benchtime/60.0f)),
+            (int)(bytes/(float)benchtime),
+            speed,
+            failed);
+    }
+    
+    return i;
+}
+
+void benchcore(const char *host,const int port,const char *req)
+{
+    int rlen;
+    char buf[1500];
+    int s,i;
+    struct sigaction sa;
+
+    /* setup alarm signal handler */
+    sa.sa_handler=alarm_handler;
+    sa.sa_flags=0;
+	//设置信号的信号处理函数
+    if(sigaction(SIGALRM,&sa,NULL))
+        exit(3);
+    
+	//设置闹钟函数,默认动作是benchtime时间后，终止调用alarm函数的进程。
+    alarm(benchtime); // after benchtime,then exit
+
+    rlen=strlen(req);
+    nexttry:
+	while(1)
+    {
+		//默认为0，收到信号则为1
+        if(timerexpired)
+        {
+            if(failed>0)
+            {
+                /* fprintf(stderr,"Correcting failed by signal\n"); */
+                failed--;
+            }
+            return;
+        }
+        
+		//创建连接
+        s=Socket(host,port);                          
+        if(s<0) { failed++;continue;}
+		//向服务器发送http请求
+        if(rlen!=write(s,req,rlen)) {failed++;close(s);continue;}
+        if(http10==0) //http 0.9 
+			if(shutdown(s,1)) //如果关闭不成功
+			{
+				failed++;
+				close(s);
+				continue;
+			}
+        if(force==0)  //不传-f选项，默认为0，等待服务器响应 
+        {
+            /* read all available data from socket */
+            while(1)
+            {
+                if(timerexpired) break; 
+                i=read(s,buf,1500);
+                /* fprintf(stderr,"%d\n",i); */
+                if(i<0) //从服务器读取数据失败
+                { 
+					//关闭当前连接，重新创建新连接
+                    failed++;
+                    close(s);
+                    goto nexttry;
+                }
+                else
+					if(i==0)
+						break;
+					else
+						bytes+=i;
+            }
+        }
+        if(close(s)) {failed++;continue;}
+        speed++;  //成功访问一次服务器，speed＋1
+    }
+}
